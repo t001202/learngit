9a9ce1893de9ba856d4a2a5a69b1fabf64a2f3ee
wangyaxi <WangYx9188@gmail.com>
2017-09-01 10:27:35 +0800

    添加mongodb

---
+++ springboot/ch803/src/main/java/com/wyx/boot/Ch803Application.java
@@ -0,0 +1,12 @@
+package com.wyx.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch803Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch803Application.class, args);
+	}
+}
